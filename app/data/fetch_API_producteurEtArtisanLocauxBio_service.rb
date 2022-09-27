d74ec5cb0ea25f948d8859b4356cc626da123d2b
Kenkaneki-42 <sylvain.vandermeersch@gmail.com>
2022-09-27 02:58:00 +0200

    add a user_interface that will become a method for the controller shop to interact with the view, add a shop's class which will became a model,add a fetch fil for fetching shop service with API and a apptest.rb to test it

--- app/data/fetch_API_producteurEtArtisanLocauxBio_service.rb
+++ app/data/fetch_API_producteurEtArtisanLocauxBio_service.rb
@@ -1,6 +1,6 @@
 require "json"
 require "open-uri"
-
+require_relative "shop"
 # Appel de toute la data: endpoint principal: https://opendata.agencebio.org/api/gouv/operateurs/
 # url = "https://opendata.agencebio.org/api/gouv/operateurs"
 
@@ -14,3 +14,25 @@ require "open-uri"
 
 # Version où l'on place en paramètre dans l'url la ville:
 url = "https://opendata.agencebio.org/api/gouv/operateurs/?q="
+
+
+# We fetch shop data from API
+# we instantiate a shop with this data
+def fetch_shop_service(shop_city, shop_category)
+  # build url https://pokeapi.co/api/v2/pokemon/10
+  url = "https://pokeapi.co/api/v2/pokemon/#{pokemon_number}"
+  # we call api endpoint that response with a json
+  response = URI.open(url).read
+  # we parse json
+  data = JSON.parse(response)
+  # we extract data from parsed json
+  name = data["name"]
+  height = data["height"]
+  weight = data["weight"]
+  picture_url = data["sprites"]["front_default"]
+  types = data["types"].map do |type_data|
+    type_data["type"]["name"]
+  end # => ["grass", "poison"]
+  # and we build a pokemon from it
+  Pokemon.new(pokemon_number, name, height, weight, picture_url, types)
+end
---
+++ app/data/shop.rb
@@ -0,0 +1,20 @@
+class Shop
+  attr_reader :picture_url
+
+  def initialize(id, name, height, weight, picture_url, types)
+    @id = id
+    @name = name
+    @height = height
+    @weight = weight
+    @picture_url = picture_url
+    @types = types
+  end
+
+  def to_s
+    # HEREDOC syntax
+    <<~STR
+    (#{@id}) #{@name}. It weights #{@weight.fdiv(10)}kg for \
+    a height of #{@height / 10.0}m. Its types are: #{@types.join(', ')}.
+    STR
+  end
+end
